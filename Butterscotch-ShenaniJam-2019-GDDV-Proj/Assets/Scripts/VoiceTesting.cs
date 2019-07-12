b05ea97470501dc6ee9cf907ad725ce0b8aaba71
FTWRahul <r.poundarik@gmail.com>
2019-07-12 17:54:08 -0400

    Idk what I did here but it's imortant

--- Butterscotch-ShenaniJam-2019-GDDV-Proj/Assets/Scripts/VoiceTesting.cs
+++ Butterscotch-ShenaniJam-2019-GDDV-Proj/Assets/Scripts/VoiceTesting.cs
@@ -20,11 +20,11 @@ public class VoiceTesting : MonoBehaviour
 
     private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
     {
-        Debug.Log(args.text);
-        StringBuilder builder = new StringBuilder();
-        builder.AppendFormat("{0} ({1}){2}", args.text, args.confidence, Environment.NewLine);
-        builder.AppendFormat("\tTimestamp: {0}{1}", args.phraseStartTime, Environment.NewLine);
-        builder.AppendFormat("\tDuration: {0} seconds{1}", args.phraseDuration.TotalSeconds, Environment.NewLine);
-        Debug.Log(builder.ToString());
+        Debug.Log(args.text + " " + "("+ args.confidence + ")");
+        //StringBuilder builder = new StringBuilder();
+        //builder.AppendFormat("{0} ({1}){2}", args.text, args.confidence, Environment.NewLine);
+        //builder.AppendFormat("\tTimestamp: {0}{1}", args.phraseStartTime, Environment.NewLine);
+        //builder.AppendFormat("\tDuration: {0} seconds{1}", args.phraseDuration.TotalSeconds, Environment.NewLine);
+        //Debug.Log(builder.ToString());
     }
 }
