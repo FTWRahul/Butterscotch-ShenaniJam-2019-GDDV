83e0110b52e5e981a0a30e886b251739b7514252
FTWRahul <r.poundarik@gmail.com>
2019-07-12 16:31:46 -0400

    Voice success

---
+++ Butterscotch-ShenaniJam-2019-GDDV-Proj/Assets/Scripts/VoiceTesting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceTesting : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecog;
+    [SerializeField]
+    List<string> keywordList;
+
+    void Start()
+    {
+        keywordRecog = new KeywordRecognizer(keywordList.ToArray());
+        keywordRecog.OnPhraseRecognized += OnPhraseRecognized;
+        keywordRecog.Start();
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        Debug.Log(args.text);
+        StringBuilder builder = new StringBuilder();
+        builder.AppendFormat("{0} ({1}){2}", args.text, args.confidence, Environment.NewLine);
+        builder.AppendFormat("\tTimestamp: {0}{1}", args.phraseStartTime, Environment.NewLine);
+        builder.AppendFormat("\tDuration: {0} seconds{1}", args.phraseDuration.TotalSeconds, Environment.NewLine);
+        Debug.Log(builder.ToString());
+    }
+}
