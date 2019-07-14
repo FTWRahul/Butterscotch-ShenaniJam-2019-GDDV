4d88e07878da20b8b0cff2b5e55f37e5d6242feb
Jane <glazychevaeo@gmail.com>
2019-07-14 14:58:43 -0400

    Revert "Santa stuff"
    This reverts commit 73fd81d08a092a0e7487a17fdc94da7ae36688e9.

--- Butterscotch-ShenaniJam-2019-GDDV-Proj/Assets/Scripts/PlayerIdentity.cs
+++ Butterscotch-ShenaniJam-2019-GDDV-Proj/Assets/Scripts/PlayerIdentity.cs
@@ -6,36 +6,20 @@ using UnityEngine.Networking;
 public class PlayerIdentity : NetworkBehaviour
 {
     public GameObject playerPersonPrefab;
-    public GameObject santaPersonPrefab;
-    public bool isSanta;
 
     void Start()
     {
         if (hasAuthority)
         {
-            if (NetworkServer.connections.Count == 1)
-            {
-                CmdSpawnMySanta();
-            }
-            else
-            {
-                CmdSpawnMyPlayer();
-            }
+            CmdSpawnMyUnit();
         }
     }
 
 
     [Command]
-    void CmdSpawnMyPlayer()
+    void CmdSpawnMyUnit()
     {
         GameObject go = Instantiate(playerPersonPrefab);
-        NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
-    }
-
-    [Command]
-    void CmdSpawnMySanta()
-    {
-        GameObject go = Instantiate(santaPersonPrefab);
         NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
     }
 }
