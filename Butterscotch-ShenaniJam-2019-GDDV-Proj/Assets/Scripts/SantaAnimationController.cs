9df362e87b56a00020c8f9314c109d75f55e64e9
Jane <glazychevaeo@gmail.com>
2019-07-14 18:21:10 -0400

    Santa animation

---
+++ Butterscotch-ShenaniJam-2019-GDDV-Proj/Assets/Scripts/SantaAnimationController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SantaAnimationController : MonoBehaviour
+{
+    Animator anim;
+
+    CharacterController moveCont;
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        moveCont = GetComponentInParent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        anim.SetBool(0, moveCont.isGrounded);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            anim.SetTrigger(2);
+            anim.SetFloat(5, Random.Range(0, 1));
+        }
+
+        //anim.SetFloat(3,horizontal(-1,1));
+        //anim.SetFloat(4,vertical(-1,1));
+    }
+}
