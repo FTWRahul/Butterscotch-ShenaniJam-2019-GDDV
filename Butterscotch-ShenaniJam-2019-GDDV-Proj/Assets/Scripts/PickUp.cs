e79ebf659a8dfd90aa9c9181f29a6e35d7399406
Jane <glazychevaeo@gmail.com>
2019-07-14 12:29:52 -0400

    Added hud

--- Butterscotch-ShenaniJam-2019-GDDV-Proj/Assets/Scripts/PickUp.cs
+++ Butterscotch-ShenaniJam-2019-GDDV-Proj/Assets/Scripts/PickUp.cs
@@ -1,24 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickUp : MonoBehaviour
 {
+    Vector2 dotPos = new Vector2(625, 320);
+
+    bool isChecking;
+
+    int checkAmount;
+
+
+
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 100.0f))
+            if (isChecking)
             {
-                if (hit.collider.tag == "Gift")
-                {
-                    Debug.Log("You are collecting this gift");
-                }
+                
+                // open slider
+                // generate pos
+                //start moving
+                // Vector2 pos = slider dot pos;
+                // if(pos in the area){
             }
+            else
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(dotPos);
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    if (hit.collider.tag == "Gift")
+                    {
+                        Debug.Log("You are collecting this gift");
+                        isChecking = true;
+                        
+                        // if player press F and he is now in skill check
+                        //check position of Point in this moment
+                        //if this point in the area open next skill check
+                        // if not close slider
+                        //make sound
+                        //if it was last skill check and player did it right
+                        // destroy gift and add it to amount 
+                    }
+                }
+
+                checkAmount = Random.Range(2, 5);
+            }
+            
         }
+
 
     }
 }
