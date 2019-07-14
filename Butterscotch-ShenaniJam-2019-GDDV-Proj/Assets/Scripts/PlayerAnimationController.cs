6a3d88c98bad8d415f8e065624123d61251ff706
Jane <glazychevaeo@gmail.com>
2019-07-14 18:09:48 -0400

    Player Animation

---
+++ Butterscotch-ShenaniJam-2019-GDDV-Proj/Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    Animator anim;
+
+    CharacterController moveCont;    
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        moveCont = GetComponentInParent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        anim.SetBool(0, moveCont.isGrounded);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger(1);
+        }
+
+        //if(isDead)
+        //{
+        //anim.SetTrigger(2);
+        //}
+
+        //if(recived hit)
+        //{
+        //    anim.SetTrigger(3);
+        //}
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            anim.SetTrigger(4);
+            anim.SetFloat(7, Random.Range(0, 1));
+        }
+
+        //anim.SetFloat(5,horizontal(-1,1));
+        //anim.SetFloat(6,vertical(-1,1));
+    }
+}
