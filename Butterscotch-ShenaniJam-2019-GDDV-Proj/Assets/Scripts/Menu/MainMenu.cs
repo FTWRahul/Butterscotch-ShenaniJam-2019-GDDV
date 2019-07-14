adf35e71545b33e80da69d933b75f8da16e0131b
Jane <glazychevaeo@gmail.com>
2019-07-14 01:08:58 -0400

    Updated Scene_Menu

--- Butterscotch-ShenaniJam-2019-GDDV-Proj/Assets/Scripts/Menu/MainMenu.cs
+++ Butterscotch-ShenaniJam-2019-GDDV-Proj/Assets/Scripts/Menu/MainMenu.cs
@@ -5,15 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        SceneManager.LoadScene(1, LoadSceneMode.Additive);
-    }
+    public Camera cam;
 
-    // Update is called once per frame
+    public Transform firstPoint;
+    public Transform secondPoint;
+
+    public float turningRate = 150f;
+    public float speed = 150;
+
     void Update()
     {
-        
+      
+   
     }
+
+    public void SettingsMenuMove()
+    {
+        cam.transform.position = Vector3.Lerp(transform.position, secondPoint.position, speed * Time.deltaTime);
+        cam.transform.rotation = Quaternion.RotateTowards(transform.rotation, secondPoint.rotation, turningRate * Time.deltaTime);
+    } 
+
+    public void MainMenuMove()
+    {
+        cam.transform.position = Vector3.Lerp(firstPoint.position, firstPoint.position, speed * Time.deltaTime);
+        cam.transform.rotation = Quaternion.RotateTowards(transform.rotation, firstPoint.rotation, turningRate * Time.deltaTime);
+    }
+
 }
