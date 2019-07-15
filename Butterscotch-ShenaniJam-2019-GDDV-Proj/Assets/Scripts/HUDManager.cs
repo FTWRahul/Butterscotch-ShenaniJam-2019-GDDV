90ff4e0672e5a99f3104c06662a81c6f42563275
CubeMD <markelovdp@gmail.com>
2019-07-15 03:50:36 -0400

    hud

---
+++ Butterscotch-ShenaniJam-2019-GDDV-Proj/Assets/Scripts/HUDManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+
+public class HUDManager : MonoBehaviour
+{
+    public TextMeshProUGUI playerCount;
+    public TextMeshProUGUI giftsRemaining;
+
+
+    private void Update()
+    {
+        playerCount.text = (GameObject.FindGameObjectsWithTag("Player").Length + 1).ToString();
+        giftsRemaining.text = GameObject.FindGameObjectsWithTag("Gift").Length.ToString();
+    }
+}
