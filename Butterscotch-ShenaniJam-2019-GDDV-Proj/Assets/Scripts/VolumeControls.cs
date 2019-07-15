de4f168ee4aeda6776fc517d0cd0694f251addea
Amanda <amandaduarte2012@gmail.com>
2019-07-14 20:59:51 -0400

    Added and set up Audio Mixers
    For All the volumes
    SFXs
    Music
    and Voice Control

---
+++ Butterscotch-ShenaniJam-2019-GDDV-Proj/Assets/Scripts/VolumeControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeControls : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+
+
+    public void SetSfxLv(float sfxLvl)
+    {
+        masterMixer.SetFloat("sfxVol", sfxLvl);
+    }
+
+    public void SetMusicLv(float musicLvl)
+    {
+        masterMixer.SetFloat("musicVol", musicLvl);
+    }
+
+    public void SetVoiceContLv(float voiceContLvl)
+    {
+        masterMixer.SetFloat("voiceContVol", voiceContLvl);
+    }
+
+}
